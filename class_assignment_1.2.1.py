27b83b92766d751679d63b7829554554da7fdd05
RAKSHITH2224 <99284605+RAKSHITH2224@users.noreply.github.com>
2022-03-06 13:35:30 +0530

    Add files via upload

---
+++ class_assignment_1.2.1.py
@@ -0,0 +1,36 @@
+
+print("1.Add")
+print("2.Subtract")
+a=int(input("Enter the option "))
+v1=int(input("Enter the value 1: "))
+v2=int(input("Enter the value 2: "))
+
+if (a==1):
+    print ("Output:",v1+v2)
+
+else:
+    print ("Output:",v1-v2)
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+c=int(input("enter number:"))
+
+if(a>b)and(a>c):
+    print("a is greater")
+
+elif(b>a)and(b>c):
+    print("b is greater")
+
+else:
+    print("c is greater")
+
+
+if(a%5==0 or a%10==0):
+    print("number is either multiple of 5 or 10")
+
+else:
+    print("number is neither multiple of 5 nor 10")
+    
+if(a != 10):
+    print("number is 10")
+
+3
